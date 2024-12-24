87775bafdc862ea8e14c87cd28bbb449ead6a6d4
Luiz <luizfernando_leite@hotmail.com>
2024-12-24 17:11:22 -0300

    Creating the  payment rote

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, jsonify
+from repository.database import db
+from db_models.payments import Payment
+
+app = Flask(__name__)
+
+
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///database.db'
+app.config['SECRET_KEY']='SECRET_KEY_WEBSOCKET'
+
+db.init_app(app)
+
+@app.route('/payments/pix', methods=['POST'])
+def create_payment_pix():
+    return jsonify({"message" : "The payment has been created"})
+
+@app.route('/payments/pix/confirmation', methods=['POST'])
+def pix_confirmation():
+    return jsonify({"message" : "The payment has been confirmed"})
+
+@app.route('/payments/pix/<int:payment_id>', methods=['GET'])
+def payment_pix_page(payment_id):
+    return 'pagamento_pix'
+
+if __name__ == '__main__':
+    app.run(debug=True)
